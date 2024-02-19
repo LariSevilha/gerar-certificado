8f411fe37ecbb1a8690f66962433ea4194413a16
Larissa Sevilha <sevilha.larissa10@gmail.com>
2024-02-19 10:06:39 -0400

    receive value of the input

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ window.onload = function(){
    
 
 }
+
+window.onload = function() {
+    var btnGerarCertificado = document.getElementById("btnGerarCertificado");
+    
+    btnGerarCertificado.onclick = function() {
+        var nomeParticipante = document.getElementById("inputNome").value;
+        var tituloCurso = document.getElementById("inputNomeCurso").value;
+        var nomeMinistrante = document.getElementById("inputMinistrante").value;
+        var dataCurso = document.getElementById("inputDataCurso").value;
+        var cargaHoraria = document.getElementById("inputCargaHoraria").value;
+    }
+};
+
+console.log(nomeParticipante)
